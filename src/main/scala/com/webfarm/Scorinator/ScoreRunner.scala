18a90f15e46fc9a73a90026751407f8461ec7c98
Larry Mills-Gahl <elemgee@gmail.com>
2020-05-13 17:36:38 -0500

    cleaning up MVP and minimal output
    Signed-off-by: Larry Mills-Gahl <elemgee@gmail.com>

--- src/main/scala/com/webfarm/Scorinator/ScoreRunner.scala
+++ src/main/scala/com/webfarm/Scorinator/ScoreRunner.scala
@@ -1,8 +1,7 @@
 package com.webfarm.Scorinator
 
 import org.slf4j.LoggerFactory
-
-import java.io.File
+import java.io.{File, FileNotFoundException}
 
 object ScoreRunner {
   val logger = LoggerFactory.getLogger("ScoreRunner")
@@ -10,15 +9,29 @@ object ScoreRunner {
   def main(args: Array[String]): Unit = {
     logger debug "initializing ScoreRunner"
     try {
-      val fname = args(0)
-      val file2score = new File(fname)
-      val scorer = new NameScorer(Left(file2score), NameScorer.simpleScore)
+      val f = new File(args(0))
+      val scorer = new NameScorer(Left(f), NameScorer.simpleScore)
+      val filescore = scorer.score4Names
+      println(f"""\nThe total score for names in ${f.getName} is ${filescore}%,d""")
+      println(
+        f"""
+           |     file to score: ${f.getAbsolutePath}
+           |     names in file: ${scorer.names2score.size}
+           |       total score: ${filescore}%,d
+           |
+           |
+           |""".stripMargin)
 
 
     } catch {
       case bounds: ArrayIndexOutOfBoundsException => {
-        logger error "You must pass the filename (with complete path) as an argument to ScoreRunner"
-        logger error bounds.getCause.toString
+        println("You must specify a file containing names to be scored")
+        println(usage)
+      }
+      case fnf: FileNotFoundException => {
+        println("The file you passed on the command line could not be found")
+        println(fnf.getMessage)
+        println(usage)
       }
       case t: Throwable => {
         t.printStackTrace()
@@ -28,6 +41,20 @@ object ScoreRunner {
 
     logger debug "finished ScoreRunner"
   }
+
+
+  lazy val usage =
+    s"""
+  =========
+  ScoreRunner
+
+  This is a command line wrapper to calculate and sum scores for a list of names.
+  Pass in a file with a comma separated list of names to be scored
+
+  Usage: java -jar scorinator.jar FILE_TO_SCORE
+
+
+      """.stripMargin
 
 }
 
