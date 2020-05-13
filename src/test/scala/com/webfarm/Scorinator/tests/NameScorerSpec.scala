6f816a81ae76468918c3256fb47ae3a56ccdb25e
Larry Mills-Gahl <elemgee@gmail.com>
2020-05-13 15:32:48 -0500

    WIP: setup funspec
    Signed-off-by: Larry Mills-Gahl <elemgee@gmail.com>

--- src/test/scala/com/webfarm/Scorinator/tests/NameScorerSpec.scala
+++ src/test/scala/com/webfarm/Scorinator/tests/NameScorerSpec.scala
@@ -1,36 +1,40 @@
 package com.webfarm.Scorinator.tests
 
-import org.scalatest.flatspec.AnyFlatSpec
-import com.webfarm.Scorinator._
-import org.slf4j.LoggerFactory
-import java.io.File
 
-class NameScorerSpec extends AnyFlatSpec {
+
+import org.slf4j.LoggerFactory
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.funspec.AnyFunSpec
+
+import java.io.File
+import com.webfarm.Scorinator._
+
+class NameScorerSpec extends AnyFunSuite {
 
   import NameScorerSpec._
 
 
-  s""""${linda} in sort position ${lindaOrder}"""" should s"receive a score of ${lindaScore}" in {
+  test(s""""${linda}" in sort position ${lindaOrder} should receive a score of ${lindaScore}""") {
     assert(NameScorer.simpleScore(linda, lindaOrder) == lindaScore)
   }
 
-  s""""${linda}"""" should s"""be at sort position ${lindaOrder} (array index ${lindaOrder - 1}) in the test array""" in {
+ test(s""""${linda}" should be at sort position ${lindaOrder} (array index ${lindaOrder - 1}) in the test array"""){
     val sorted = names.sorted
     assert(linda.equals(sorted(lindaOrder - 1)))
   }
 
-  s"""the total score for the test list""" should f"""be $namesScore%,d""" in {
+  test(f"""the total score for the test list be $namesScore%,d""" ) {
     val sorted = names.sorted
     val nameScores =  1 to sorted.length map  ( idx => NameScorer.simpleScore(sorted(idx - 1 ), idx))
     assert(nameScores.sum == namesScore)
   }
 
-  s"""creating a NameScorer with a list of strings""" should f"succeed and have a score4Names of $namesScore%,d" in {
+    test(f"""creating a NameScorer with a list of strings should succeed and have a score4Names of $namesScore%,d""") {
     val scorer = new NameScorer(Right(names), NameScorer.simpleScore)
     assert(scorer.score4Names == namesScore)
   }
 
-  "creating a NameScorer with a File" should f"succeed and have a score4Names of $namesFileScore%,d" in {
+  test(f"creating a NameScorer with a File should succeed and have a score4Names of $namesFileScore%,d") {
     val testfile = getClass().getResource("/names.txt")
     val scorer = new NameScorer(Left(new File(testfile.toURI)), NameScorer.simpleScore)
     assert(scorer.score4Names == namesFileScore)
